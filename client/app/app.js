9de4dfc7379d3d6ae5b066880809423a59216022
Andrew Switlyk <andrewcswitlyk@gmail.com>
2014-08-25 19:14:29 -0700

    Added routing and blank html pages that routing points to. Added navbar links to new rounting.

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,38 @@
-var selfies = angular.module('selfies', []);
+var selfies = angular.module('selfies', ['ngRoute'])
+	.config(['$routeProvider', '$locationProvider',
+		function($routeProvider, $locationProvider) {
+		  $routeProvider
+		  	.when('/signup', {
+		  		templateUrl: 'templates/signup.html',
+		  		controller: 'loginCtrl'
+		  	})
+		    .when('/login', {
+		    	templateUrl: 'templates/login.html',
+		    	controller: 'loginCtrl'
+		    })
+		    .when('/upload', {
+		    	templateUrl: 'templates/upload.html',
+		    	controller: 'selfiesCtrl'
+		    })
+		    .when('/MostLiked', {
+		    	templateUrl: 'templates/mostliked.html',
+		    	controller: 'selfiesCtrl'
+		    })
+		    .when('/MostRecent', {
+		    	templateUrl: 'templates/mostrecent.html',
+		    	controller: 'selfiesCtrl'
+		    })
+		    .when('/useradmin', {
+		    	templateUrl: 'template/useradmin.html',
+		    	controller: 'loginCtrl'
+		    })
+		    .otherwise({
+		    	templateUrl: 'templates/splash.html',
+		    	controller: 'selfiesCtrl'
+		    });
+		  // html5Mode allows your routes to not have a hash
+		  $locationProvider.html5Mode(true);
+		}]);
 
 selfies.controller('selfiesCtrl', function($scope) {
 	$scope.test = 'hello world!';
